1bcfa0730eadc50e3ea9143a5c7ecfaa5de547fd
Judith Onyejekwe <onyejekwejudith@yahoo.com>
2026-06-12 14:37:45 +0000

    2 sums

---
+++ two_sums.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    float x;
+    float y;
+
+    std::cout << "Enter first number: "<< std::endl;
+    std::cin>> x;
+    std::cout << "Enter second number: "<< std::endl;
+    std::cin>> y;
+
+    std::cout<< "The sum is " << x+y<< std::endl;
+
+
+
+    return 0;
+}
